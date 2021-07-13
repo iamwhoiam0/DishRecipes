f4fa12c25ca3176abea79919d167721fe1160e5d
iamwhoiam000 <gamzatgasanov2337@mail.ru>
2021-07-13 17:27:29 +0300

    add fragment details

---
+++ app/src/main/java/com/example/dishrecipes/view/fragments/DishDetailsFragment.kt
@@ -0,0 +1,36 @@
+package com.example.dishrecipes.view.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.dishrecipes.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [DishDetailsFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class DishDetailsFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dish_details, container, false)
+    }
+
+}
